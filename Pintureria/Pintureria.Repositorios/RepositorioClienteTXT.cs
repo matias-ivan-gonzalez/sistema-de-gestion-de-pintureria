f345fb2cc5ab6c0ccd2b78acf7cdfddb97847364
Matias Gonzalez <MatiasG2001@outlook.com>
2022-10-11 22:55:35 -0300

    envio estructura de repositorios

--- Pintureria/Pintureria.Repositorios/RepositorioClienteTXT.cs
+++ Pintureria/Pintureria.Repositorios/RepositorioClienteTXT.cs
@@ -3,7 +3,16 @@ using Pintureria.Aplicacion;
 public class RepositorioClienteTXT : IRepositorioCliente {
     public RepositorioClienteTXT(){}
 
-    
-
-
+    public bool add(Cliente cli){
+        return false;
+    }
+    public bool modify(Cliente cli){
+        return false;
+    }
+    public bool delete(int id){
+        return false;
+    }
+    public bool get(){
+        return false;
+    }
 }
---
+++ Pintureria/Pintureria.Repositorios/RepositorioProductoTXT.cs
@@ -0,0 +1,18 @@
+namespace Pintureria.Repositorios;
+using Pintureria.Aplicacion;
+public class RepositorioProductoTXT : IRepositorioProducto {
+    public RepositorioProductoTXT(){}
+
+    public bool add(Producto pro){
+        return false;
+    }
+    public bool modify(Producto pro){
+        return false;
+    }
+    public bool delete(int id){
+        return false;
+    }
+    public bool get(){
+        return false;
+    }
+}
