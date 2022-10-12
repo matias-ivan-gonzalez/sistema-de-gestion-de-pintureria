75370c1994a9f2c72947f8775c1a8f7d33e75cdb
Matias Gonzalez <MatiasG2001@outlook.com>
2022-10-11 22:38:36 -0300

    se elimina clase no usada

--- Pintureria/Pintureria.Aplicacion/useCases/UseCase.cs
+++
@@ -1,8 +0,0 @@
-namespace Pintureria.Aplicacion;
-
-public class UseCase{
-    protected readonly IRepositorio repositorio;
-    public UseCase(IRepositorio unRepositorio){
-        repositorio = unRepositorio;
-    }
-}
