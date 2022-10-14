36b576a69fd1f46586a987ec0fad0653520ae758
Matias Gonzalez <MatiasG2001@outlook.com>
2022-10-14 17:02:54 -0300

    refactors al crud de clientes

---
+++ Pintureria/Pintureria.Repositorios/repositorios/FileHelper.cs
@@ -0,0 +1,110 @@
+using Pintureria.Aplicacion;
+class FileHelper{
+    string path = "../Pintureria.Repositorios/recursos/clientes.txt";
+
+    public void sobreEscribirArchivoPorAlteracion(string? archivo, string? actual, Cliente cli){
+            try{
+                if(actual != null){
+                    if (clienteContieneId(actual, cli.Id)){
+                        archivo = archivo?.Replace(actual,cli.ToString());
+                        File.WriteAllText(path,archivo);
+                    }
+                }
+            }
+            catch (System.Exception e){
+                Console.WriteLine(e.Message);
+            }
+        }
+
+    public void agregarClienteNoExistente(string? cliente, Cliente cli){
+        StreamWriter? streamWriter = null;
+        try{
+            if(cliente == null){
+                streamWriter = new StreamWriter(path, true);
+                streamWriter.WriteLine($"{cli.ToString()} ");
+            } 
+        }
+        catch (System.Exception e){
+            Console.WriteLine(e.Message);
+        }
+        finally{
+            streamWriter?.Close();
+        } 
+    }
+
+    public bool clienteContieneId(string? actual, int id){
+        return (actual !=null && actual.Contains($"ID: {id}"));
+    }
+
+    public string? obtenerArchivoCompleto(){
+        StreamReader? streamReader = null;
+        string? archivo = null;
+        try{
+            streamReader = new StreamReader(path);
+            archivo = streamReader.ReadToEnd();
+        }
+        catch (System.Exception e){
+            Console.WriteLine(e.Message);
+        }
+        finally{
+            streamReader?.DiscardBufferedData();
+            streamReader?.BaseStream.Seek(0, SeekOrigin.Begin);
+            streamReader?.Dispose();
+        }
+        return archivo;
+    }
+
+    public string? buscarCliente(int id){
+        string? actual = null;
+        try{
+            StreamReader streamReader = new StreamReader(path);
+            bool encontrado = false;
+            while(!encontrado && !streamReader.EndOfStream){
+                actual = streamReader.ReadLine();
+                    if (clienteContieneId(actual, id)){
+                        encontrado = true;
+                    }
+                }
+            streamReader.Close();
+            if(encontrado){
+                return actual;
+            }
+        }
+        catch (System.Exception e){
+            Console.WriteLine(e.Message);
+        }
+        return null;
+    }
+
+    public void eliminarCliente(int id, List<string> archivo){
+        try{
+            archivo.Remove(archivo.Where(archivo => clienteContieneId(archivo,id)).First());
+            File.WriteAllLines(path, archivo.ToArray());  
+        }
+        catch (System.Exception e){
+            Console.WriteLine(e.Message);
+        }
+        
+    }
+
+    public List<string> obtenerEntidadesDeArchivoEnLista(){
+        string? actual = null;
+        StreamReader? streamReader = null;
+        List<string> clientes = new List<string>();
+        try{
+            streamReader = new StreamReader(path);
+            while(!streamReader.EndOfStream){
+                actual = streamReader.ReadLine();
+                if(actual != null) clientes.Add(actual);
+            }
+
+        }
+        catch (System.Exception e){
+            Console.WriteLine(e.Message);
+        }
+        finally{
+            streamReader?.Dispose();
+        }
+        return clientes;
+    }
+}
--- Pintureria/Pintureria.Repositorios/repositorios/RepositorioClienteTXT.cs
+++ Pintureria/Pintureria.Repositorios/repositorios/RepositorioClienteTXT.cs
@@ -1,129 +1,28 @@
-﻿using System.Text.RegularExpressions;
-namespace Pintureria.Repositorios;
+﻿namespace Pintureria.Repositorios;
 using Pintureria.Aplicacion;
 public class RepositorioClienteTXT : IRepositorioCliente {
-    string path = "../Pintureria.Repositorios/recursos/clientes.txt";
-
+    FileHelper fileHelper = new FileHelper();
     public RepositorioClienteTXT(){}
+
     public void add(Cliente cli){
-        string? cliente = buscarCliente(cli.Id);
-        agregarClienteNoExistente(cliente, cli);
+        string? cliente = fileHelper.buscarCliente(cli.Id);
+        fileHelper.agregarClienteNoExistente(cliente, cli);
     }
+
     public void modify(Cliente cli){
-        try{
-            string? archivo = obtenerArchivoCompleto();
-            string? actual = buscarCliente(cli.Id);
-            sobreEscribirArchivoPorAlteracion(archivo,actual,cli);
-        }
-        catch (System.Exception e){
-            Console.WriteLine(e.Message);
-        }
+        string? archivo = fileHelper.obtenerArchivoCompleto();
+        string? actual = fileHelper.buscarCliente(cli.Id);
+        fileHelper.sobreEscribirArchivoPorAlteracion(archivo,actual,cli);
     }
     public void delete(int id){
-        try{
-            string? cliente = buscarCliente(id);
-            if(cliente != null){
-                List<string> archivo = get();
-                archivo.Remove(archivo.Where(archivo => clienteContieneId(archivo,id)).First());
-                File.WriteAllLines(path, archivo.ToArray());
-            }
-        }
-        catch (System.Exception e){
-            Console.WriteLine(e.Message);
+        string? cliente = fileHelper.buscarCliente(id);
+        if(cliente != null){
+            List<string> archivo = get();
+            fileHelper.eliminarCliente(id,archivo);
         }
     }
+
     public List<string> get(){
-        string? actual = null;
-        StreamReader? streamReader = new StreamReader(path);
-        List<string?> clientes = new List<string?>();
-        try{
-            while(!streamReader.EndOfStream){
-                actual = streamReader.ReadLine();
-                clientes.Add(actual);
-            }
-        }
-        catch (System.Exception e){
-            Console.WriteLine(e.Message);
-        }
-        finally{
-            streamReader?.Dispose();
-        }
-
-        return clientes;
-    }
-
-    void sobreEscribirArchivoPorAlteracion(string? archivo, string? actual, Cliente cli){
-        try{
-            if(actual != null){
-                if (clienteContieneId(actual, cli.Id)){
-                    archivo = archivo?.Replace(actual,cli.ToString());
-                    File.WriteAllText(path,archivo);
-                }
-            }
-        }
-        catch (System.Exception e){
-            Console.WriteLine(e.Message);
-        }
-    }
-
-    void agregarClienteNoExistente(string? cliente, Cliente cli){
-        StreamWriter? streamWriter = null;
-        try{
-            if(cliente == null){
-                streamWriter = new StreamWriter(path, true);
-                streamWriter.WriteLine($"{cli.ToString()} ");
-            } 
-        }
-        catch (System.Exception e){
-             Console.WriteLine(e.Message);
-        }
-        finally{
-            streamWriter?.Close();
-        }
-        
-    }
-
-    bool clienteContieneId(string? actual, int id){
-        return (actual !=null && actual.Contains($"ID: {id}"));
-    }
-
-    string? obtenerArchivoCompleto(){
-        StreamReader? streamReader = null;
-        string? archivo = null;
-        try{
-            streamReader = new StreamReader(path);
-            archivo = streamReader.ReadToEnd();
-        }
-        catch (System.Exception){
-            
-        }
-        finally{
-            streamReader?.DiscardBufferedData();
-            streamReader?.BaseStream.Seek(0, SeekOrigin.Begin);
-            streamReader?.Dispose();
-        }
-        return archivo;
-    }
-
-    string? buscarCliente(int id){
-        string? actual = null;
-        try{
-            StreamReader streamReader = new StreamReader(path);
-            bool encontrado = false;
-            while(!encontrado && !streamReader.EndOfStream){
-                    actual = streamReader.ReadLine();
-                    if (clienteContieneId(actual, id)){
-                        encontrado = true;
-                    }
-                }
-            streamReader.Close();
-            if(encontrado){
-                return actual;
-            }
-        }
-        catch (System.Exception e){
-            Console.WriteLine(e.Message);
-        }
-        return null;
+        return fileHelper.obtenerEntidadesDeArchivoEnLista();
     }
 }
