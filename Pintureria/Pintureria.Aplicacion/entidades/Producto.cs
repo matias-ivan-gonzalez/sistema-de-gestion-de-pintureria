fd5e2168a4677237ce2d25a09106a5512f473ba5
Matias Gonzalez <MatiasG2001@outlook.com>
2022-10-15 19:31:31 -0300

    envio refactor a producto

--- Pintureria/Pintureria.Aplicacion/entidades/Producto.cs
+++ Pintureria/Pintureria.Aplicacion/entidades/Producto.cs
@@ -3,17 +3,13 @@ namespace Pintureria.Aplicacion;
 public class Producto {
     static int s_id = 0;
     public int Id {get;}
-    public string Descripcion {get; set;}
+    public string? Descripcion {get; set;}
     public double PrecioUnitario {get;set;}
     public int Stock {get => Stock ; 
                      set { Stock = (value < 0) ? 0 : value;}}
 
-    public Producto(string descripcion, double precio, int stock){
-        s_id++;
-        Id = s_id;
-        Descripcion = descripcion;
-        PrecioUnitario = precio;
-        Stock = stock;
+    public Producto(){
+
     }
 
     public override string ToString(){
