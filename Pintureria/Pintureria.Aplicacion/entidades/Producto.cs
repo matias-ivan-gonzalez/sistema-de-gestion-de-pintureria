9322460384c262c1f37214eb2d0309faad3c85d8
Julian Gomez <gomezjulian1995@gmail.com>
2022-10-15 20:08:28 -0300

    Ultimo Commit

--- Pintureria/Pintureria.Aplicacion/entidades/Producto.cs
+++ Pintureria/Pintureria.Aplicacion/entidades/Producto.cs
@@ -5,11 +5,11 @@ public class Producto {
     public int Id {get;}
     public string? Descripcion {get; set;}
     public double PrecioUnitario {get;set;}
-    public int Stock {get => Stock ; 
-                     set { Stock = (value < 0) ? 0 : value;}}
+    public int Stock {get; set;}
 
     public Producto(){
-
+       s_id++;
+       Id = s_id;
     }
 
     public override string ToString(){
--- Pintureria/Pintureria.Consola/Program.cs
+++ Pintureria/Pintureria.Consola/Program.cs
@@ -8,26 +8,26 @@ var eliminarCliente = new EliminarClienteUseCase(repoCliente);
 var modificarCliente = new ModificarClienteUseCase(repoCliente);
 var persona1 = new ClienteFisico()
 {
-Nombre = "juan",
-Direccion = "Diag.74 nro 123",
-Telefono = "(11)502-1111",
-DNI = 30321654
+    Nombre = "juan",
+    Direccion = "Diag.74 nro 123",
+    Telefono = "(11)502-1111",
+    DNI = 30321654
 };
 var empresa = new ClienteEmpresa()
 {
-Nombre = "Empresa SA",
-Direccion = "calle 13 nro. 123",
-Telefono = "(221)543-3456",
-CUIT = "30-12345678-1",
-SitioWeb = "www.empresaSA.com"
+    Nombre = "Empresa SA",
+    Direccion = "calle 13 nro. 123",
+    Telefono = "(221)543-3456",
+    CUIT = "30-12345678-1",
+    SitioWeb = "www.empresaSA.com"
 };
 
 var persona2 = new ClienteFisico()
 {
-Nombre = "María",
-Direccion = "calle 5 nro 1544",
-Telefono = "(221)501-9999",
-DNI = 22752412
+    Nombre = "María",
+    Direccion = "calle 5 nro 1544",
+    Telefono = "(221)501-9999",
+    DNI = 22752412
 };
 agregarCliente.Ejecutar(persona1);
 agregarCliente.Ejecutar(empresa);
@@ -42,11 +42,90 @@ eliminarCliente.Ejecutar(1);
 listarEnConsola();
 void listarEnConsola()
 {
-var lista = listarClientes.Ejecutar();
-foreach (var c in lista)
+    var lista = listarClientes.Ejecutar();
+    foreach (var c in lista)
+    {
+        Console.WriteLine(c);
+    }
+    Console.WriteLine("-----");
+}
+Console.ReadLine();
+Console.WriteLine("\n\n");
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/* Producto */
+
+var repoProducto = new RepositorioProductoTXT();
+var agregarProducto = new AgregarProductoUseCase(repoProducto);
+var listarProductos = new ListarProductosUseCase(repoProducto);
+var eliminarProducto = new EliminarProductoUseCase(repoProducto);
+var modificarProducto = new ModificarProductoUseCase(repoProducto);
+
+Producto producto1 = new Producto()
 {
-Console.WriteLine(c);
+    Descripcion = "Balde Pintura Amarilla 3L",
+    PrecioUnitario = 530.00,
+    Stock = 30
+};
+
+Producto producto2 = new Producto()
+{
+    Descripcion = "Balde Pintura Blanca 3L",
+    PrecioUnitario = 870.00,
+    Stock = 40
+};
+
+Producto producto3 = new Producto()
+{
+    Descripcion = "Rodillo grande ",
+    PrecioUnitario = 1200.00,
+    Stock = 10
+};
+
+Producto producto4 = new Producto()
+{
+    Descripcion = "Pincel para interiores",
+    PrecioUnitario = 610.00,
+    Stock = 15
+};
+
+Producto producto5 = new Producto()
+{
+    Descripcion = "Lija fina 180",
+    PrecioUnitario = 150.00,
+    Stock = 50
+};
+
+agregarProducto.Ejecutar(producto1);
+agregarProducto.Ejecutar(producto2);
+agregarProducto.Ejecutar(producto3);
+agregarProducto.Ejecutar(producto4);
+agregarProducto.Ejecutar(producto5);
+listarProductosEnConsola();
+
+// producto3.PrecioUnitario = 930.50;
+// producto3.Descripcion = "Balde Pintura Blanca 3.5L";
+// producto3.Stock = 8;
+
+// producto5.PrecioUnitario = 165.00;
+// producto5.Stock = 200;
+
+// modificarProducto.Ejecutar(producto3);
+// modificarProducto.Ejecutar(producto5);
+// listarProductosEnConsola();
+
+// eliminarProducto.Ejecutar(2);
+// listarProductosEnConsola();
+
+void listarProductosEnConsola()
+{
+    var lista = listarProductos.Ejecutar();
+    foreach (var p in lista)
+    {
+        Console.WriteLine(p);
+    }
+    Console.WriteLine("-----");
 }
-Console.WriteLine("-----");
-}
+
 Console.ReadLine();
