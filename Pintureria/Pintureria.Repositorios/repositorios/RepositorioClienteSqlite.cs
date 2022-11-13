035c2a33eddd74f65a19f7aa1caf55339c1bb951
Matias Gonzalez <MatiasG2001@outlook.com>
2022-11-13 19:22:10 -0300

    envio de modify de clientes funcionando

--- Pintureria/Pintureria.Repositorios/repositorios/RepositorioClienteSqlite.cs
+++ Pintureria/Pintureria.Repositorios/repositorios/RepositorioClienteSqlite.cs
@@ -7,23 +7,37 @@ public class RepositorioClienteSqlite : IRepositorioCliente
     public RepositorioClienteSqlite() { }
 
     public void add(Cliente cli) {
-        var cliente = context.Clientes
+        try {
+            var cliente = context.Clientes
             .Where(c => c.Id == cli.Id)
             .FirstOrDefault<Cliente>();
-        if(cliente==null) {
-            context.Clientes.Add(cli);
-            context.SaveChanges();
+            if(cliente==null) {
+                context.Clientes.Add(cli);
+                context.SaveChanges();
+            }
+            else{
+                throw new AlreadyRegisteredException();
+            }
+        }
+        catch (System.Exception e) {
+            Console.WriteLine(e.Message);
         }
     }
 
-    public void modify(Cliente cli)
-    {
-        try
-        {
-            fileHelper.modificarCliente(cli);
+    public void modify(Cliente cli) {
+        try {
+            var cliente = context.Clientes
+            .Where(c => c.Id == cli.Id)
+            .FirstOrDefault<Cliente>();
+            if(cliente!=null){
+                cliente = cli;
+                context.SaveChanges();
+            }
+            else{
+                throw new NoSuchElementException();
+            }
         }
-        catch (NoSuchElementException e)
-        {
+        catch (NoSuchElementException e) {
             Console.WriteLine(e.Message);
         }
     }
