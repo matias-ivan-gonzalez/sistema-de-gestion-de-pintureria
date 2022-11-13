fc11b845c0f195b9fcdf2d4de5726e556969bb0f
Matias Gonzalez <MatiasG2001@outlook.com>
2022-11-13 00:43:34 -0300

    modifico tipos del context

--- Pintureria/Pintureria.Repositorios/repositorios/EntidadesContext.cs
+++ Pintureria/Pintureria.Repositorios/repositorios/EntidadesContext.cs
@@ -1,9 +1,9 @@
 using Microsoft.EntityFrameworkCore;
-
+using Pintureria.Aplicacion;
 public class EntidadesContext : DbContext {
 
-	public DbSet<Alumno> Alumnos { get; set; }
-	public DbSet<Examen> Examenes { get; set; }
+	public DbSet<Cliente> Clientes { get; set; }
+	public DbSet<Producto> Productos { get; set; }
 
 	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
 		optionsBuilder.UseSqlite("data source=Entidades.db");
