ef951bb51aab2443b0ea3f8141e8a0eb40839212
Julian Gomez <gomezjulian1995@gmail.com>
2022-11-15 22:01:22 -0300

    Es esto mati?

--- Pintureria/Pintureria.Repositorios/repositorios/EntidadesContext.cs
+++ Pintureria/Pintureria.Repositorios/repositorios/EntidadesContext.cs
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 using Microsoft.EntityFrameworkCore;
 using Pintureria.Aplicacion;
 public class EntidadesContext : DbContext {
@@ -6,27 +5,19 @@ public class EntidadesContext : DbContext {
 	#nullable disable
 	public DbSet<Cliente> Clientes { get; set; }
     public DbSet<Producto> Productos { get; set; }
-=======
-// using Microsoft.EntityFrameworkCore;
-// using Pintureria.Aplicacion;
-// public class EntidadesContext<Class> : DbContext where Class : Entidad {
 
-// 	#nullable disable
-// 	public DbSet<Class> tabla { get; set; }
->>>>>>> Stashed changes
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+		optionsBuilder.UseSqlite("data source=Entidades.db");
+	}
 
-// 	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
-// 		optionsBuilder.UseSqlite("data source=Entidades.db");
-// 	}
+	protected override void OnModelCreating(ModelBuilder modelBuilder){
+        modelBuilder.Entity<ClienteFisico>()
+            .HasDiscriminator<string>("Discriminator")
+            .HasValue<ClienteFisico>("ClienteFisico");
 
-// 	protected override void OnModelCreating(ModelBuilder modelBuilder){
-//         modelBuilder.Entity<ClienteFisico>()
-//             .HasDiscriminator<string>("Discriminator")
-//             .HasValue<ClienteFisico>("ClienteFisico");
-
-//         modelBuilder.Entity<ClienteEmpresa>()
-//             .HasDiscriminator<string>("Discriminator")
-//             .HasValue<ClienteEmpresa>("ClienteEmpresa");
-//     }
+        modelBuilder.Entity<ClienteEmpresa>()
+            .HasDiscriminator<string>("Discriminator")
+            .HasValue<ClienteEmpresa>("ClienteEmpresa");
+    }
 	
-// }
+}
