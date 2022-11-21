5d16bab8b98b212af49bcc43f3d1b915dcad20b2
Julian Gomez <gomezjulian1995@gmail.com>
2022-11-21 15:52:21 -0300

    Actualizo con ultimos cambios en Blazor

--- Pintureria/Pintureria.UI/Program.cs
+++ Pintureria/Pintureria.UI/Program.cs
@@ -12,6 +12,12 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 
+builder.Services.AddTransient<AgregarClienteUseCase>();
+builder.Services.AddTransient<ListarClientesUseCase>();
+builder.Services.AddTransient<EliminarClienteUseCase>();
+builder.Services.AddTransient<ModificarClienteUseCase>();
+// builder.Services.AddTransient<ObtenerClienteUseCase>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
