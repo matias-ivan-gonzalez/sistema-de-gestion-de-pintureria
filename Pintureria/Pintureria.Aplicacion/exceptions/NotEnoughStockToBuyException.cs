0851f95e72bb0b63dbbbbec684aa833a3673593f
Matias Gonzalez <MatiasG2001@outlook.com>
2022-11-24 12:12:59 -0300

    envio venta validada con stock insuficiente

--- Pintureria/Pintureria.Aplicacion/exceptions/NotEnoughStockToBuyException.cs
+++ Pintureria/Pintureria.Aplicacion/exceptions/NotEnoughStockToBuyException.cs
@@ -1,6 +1,6 @@
 public class NotEnoughStockToBuyException : Exception{
     private static string s_mensaje = "La cantidad solicitada en la compra supera el stock total del producto";
-    public NotEnoughStockToBuyException() : base() { }
+    public NotEnoughStockToBuyException() : base(s_mensaje) { }
 
     public NotEnoughStockToBuyException(string message) : base(String.Format(s_mensaje, message)){ 
     }
